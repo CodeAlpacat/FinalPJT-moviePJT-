d7d6f7ef362fb0daad9c4e7122290af76b04cd83
JHPark <erict4f@gmail.com>
2022-05-17 17:34:05 +0900

    JH_Update: now_playing_movie_list view add

--- Rest_api/FinalPJT_django/settings.py
+++ Rest_api/FinalPJT_django/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
-    'django_seed',
 
     'allauth',
     'allauth.account',
--- Rest_api/movies/views.py
+++ Rest_api/movies/views.py
@@ -8,8 +8,9 @@ from .serializers.movie import MovieListSerializer, MovieSerializer
 from .models import Movie
 
 #날짜정보
-import datetime
-date = datetime.datetime.now()
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+date = datetime.now()
 
 #전체 영화 가져오기
 @api_view(['GET'])
@@ -42,18 +43,24 @@ def upcoming_movie_list(request):
 @api_view(['GET'])
 def nowplaying_movie_list(request):
     movies = get_list_or_404(Movie)
-    serializers = MovieListSerializer(movies, many=True)
+    serializers_1 = MovieListSerializer(movies, many=True)
     
-    for movie in serializers.data:
+    now_playing_list = []
+    # release_date 예시: 2022-05-17
+    for movie in serializers_1.data:
         if movie['release_date'][0:4] == str(date.year):
             if int(movie['release_date'][5:7]) == date.month:
                 #같은 달에 출시했다면, 현재 날짜가 2주 이상
-                if int(movie['release_date'][8:]) <= date.day and 14<= date.day <= 29:
-                    pass #리스트에 추가
-            #현재 날짜가 14일 이전이라면, 이전 달까지 계산
-            elif movie['release_date'][5:7] == (date.month - 1):
-                
-        
-    
+                if  0 < date.day - int(movie['release_date'][8:]) <= 14:
+                    #리스트에 추가
+                    now_playing_list.append(movie)
+            #현재 날짜가 14일 이전이라면, 이전 달까지 계산 ( 1달 차이 )
+            elif int(movie['release_date'][5:7]) == ((date - relativedelta(month=1)).month):
+                # 현재 날짜기준 2주 전이 개봉일 이전인지 확인
+                if (date - relativedelta(weeks=2)).month == int(movie['release_date'][5:7]):
+                    if 0 < int(movie['release_date'][8:]) - (date - relativedelta(weeks=2)).day <= 14:
+                        #리스트에 추가
+                        now_playing_list.append(movie)   
+    # serializers_2 = MovieListSerializer(now_playing_list, many=True)
 
-    return Response(serializers.data)
+    return Response(now_playing_list)
