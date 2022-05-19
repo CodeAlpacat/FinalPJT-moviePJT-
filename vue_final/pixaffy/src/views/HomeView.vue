7d17b2ee7a923ac8205faf5ca73a91cb09adcacf
JHPark <erict4f@gmail.com>
2022-05-19 23:36:28 +0900

    JH_Update: error fixed

--- vue_final/pixaffy/src/views/HomeView.vue
+++ vue_final/pixaffy/src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-<<<<<<< HEAD
     <v-dialog
       v-model="dialogDetail"
       width="800px"
@@ -24,41 +23,33 @@
         <detail-dialog :pk="1"></detail-dialog>
       </v-card>
     </v-dialog>
-=======
     <div >
       <genre-based-recommendation></genre-based-recommendation>
       <card-view></card-view>
     </div>
->>>>>>> bcb11683ae535ef6940af4beb6438ec516c79eca
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import UpcomingMovies from '@/components/UpcomingMovies.vue';
 import CardAlgorithm from '@/components/CardAlgorithm.vue';
 import DetailDialog from '@/components/DetailDialog.vue';
 import { mapGetters, mapActions } from 'vuex';
 
-=======
 import GenreBasedRecommendation from '@/components/GenreBasedRecommendation.vue';
 import CardView from '@/components/CardView.vue';
->>>>>>> bcb11683ae535ef6940af4beb6438ec516c79eca
 export default {
   name: "HomeView",
 
   components: {
-<<<<<<< HEAD
     UpcomingMovies,
     CardAlgorithm,
     DetailDialog
   },
   computed: {
-    ...mapGetters(['dialogDetail'])
-=======
+    ...mapGetters(['dialogDetail']),
     CardView,
     GenreBasedRecommendation
->>>>>>> bcb11683ae535ef6940af4beb6438ec516c79eca
   },
 
   data() {
