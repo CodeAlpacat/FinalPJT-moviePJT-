f71239eb7aefee532cc2d530b8624fb3672ad8cb
wonwoo <lww0407@naver.com>
2022-05-20 22:13:18 +0900

    WW_Update: Community Server Built

--- final_pjt_back/Rest_api/community/models.py
+++ final_pjt_back/Rest_api/community/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.conf import settings
 
 
-
+# user을 통해 게시글을 작성한 유저 / 역참조를 통한 유저가 작성한 게시글 조회 가능
+# liked_users를 통한 게시글 좋아요 및 게시글에 좋아요를 누른 유저 조회
 class Article(models.Model): # 작성한 리뷰
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='articles')
     title = models.CharField(max_length=50)
@@ -11,10 +12,13 @@ class Article(models.Model): # 작성한 리뷰
     updated_at = models.DateTimeField(auto_now=True)
     liked_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles', blank=True) # 게시글에 대한 좋아요.
 
+
+# user을 통해 댓글을 작성한 유저 / 역참조를 통한 유저가 작성한 댓글 조회 가능
+# article을 통해 댓글이 게시된 게시글 / 게시글에 댓글을 단 목록 조회 가능
+# liked_users를 통한 게시글 좋아요 / 역참조를 통한 게시글에 좋아요를 누른 유저 조회
 class Comment(models.Model): # 작성한 리뷰
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
-    title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- final_pjt_back/Rest_api/community/views.py
+++ final_pjt_back/Rest_api/community/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.db.models import Count
 
@@ -48,7 +49,10 @@ def article_detail_or_update_or_delete(request, article_pk):
     def delete_article():
         if request.user == article.user:
             article.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            data= {
+                'delete':'삭제가 완료되었습니다.'
+            }
+            return Response(data, status=status.HTTP_204_NO_CONTENT)
 
     if request.method == 'GET':
         return article_detail()
@@ -58,3 +62,80 @@ def article_detail_or_update_or_delete(request, article_pk):
     elif request.method == 'DELETE':
         if request.user == article.user:
             return delete_article()
+
+#유저가 게시글에 좋아요
+#이미 눌렀다면 유저 정보 삭제 / 없다면 좋아요
+@api_view(['POST'])
+def like_article(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    user = request.user
+    if article.liked_users.filter(pk=user.pk).exists():
+        article.liked_users.remove(user)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    else:
+        article.liked_users.add(user)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+#댓글 생성
+@api_view(['POST'])
+def create_comment(request, article_pk):
+    user = request.user
+    article = get_object_or_404(Article, pk=article_pk)
+    
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(article=article, user=user) #현재 유저와 게시글 정보 저장
+        comments = article.comments.all()#댓글을 입력하는 도중에 댓글 업데이트
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT', 'DELETE'])
+def comment_update_or_delete(request, article_pk, comment_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    comment = get_object_or_404(Comment, pk=comment_pk)
+
+    def update_comment():
+        if request.user == comment.user:
+            serializer = CommentSerializer(instance=comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                comments = article.comments.all()
+                serializer = CommentSerializer(comments, many=True)
+                return Response(serializer.data)
+    
+    def delete_comment():
+        if request.user == comment.user:
+            comment.delete()
+            comments = article.comments.all()
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        return update_comment()
+    elif request.method == 'DELETE':
+        return delete_comment()
+    
+    
+
+@api_view(['POST'])
+def like_comment(request, article_pk, comment_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    #댓글이 달린 게시글 필드 정보와 게시글의 pk가 같아야 좋아요 가능
+    user = request.user
+    serializer = CommentSerializer(comment)
+    if article.pk == serializer.data.get('article'):
+        if comment.liked_users.filter(pk=user.pk).exists():
+            comment.liked_users.remove(user)
+            serializer = CommentSerializer(comment)
+            return Response(serializer.data)
+        else:
+            comment.liked_users.add(user)
+            serializer = CommentSerializer(comment)
+            return Response(serializer.data)
+    #만약 댓글과 게시글이 다른 경우에는 203 에러 출력
+    else:
+        return Response(status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
